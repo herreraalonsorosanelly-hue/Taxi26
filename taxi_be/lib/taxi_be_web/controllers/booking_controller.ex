bf79a770e7cbd658bd53a1ebb1d8f8a8eb970d0c
Rosa <herreraalonsorosanelly@gmail.com>
2026-06-09 15:44:00 -0600

    segundo cambio  base

--- taxi_be/lib/taxi_be_web/controllers/booking_controller.ex
+++ taxi_be/lib/taxi_be_web/controllers/booking_controller.ex
@@ -1,18 +1,21 @@
 defmodule TaxiBeWeb.BookingController do
   use TaxiBeWeb, :controller
-  # alias TaxiBeWeb.TaxiAllocationJob
+  alias TaxiBeWeb.TaxiAllocationJob
+
   def create(conn, req) do
-    IO.inspect(req)
-    # booking_id = UUID.uuid1()
-    # TaxiAllocationJob.start_link(
-    #   req |> Map.put("booking_id", booking_id),
-    #   String.to_atom(booking_id)
-    # )
+    booking_id = UUID.uuid1()
+
+    TaxiAllocationJob.start_link(
+      req |> Map.put("booking_id", booking_id),
+      String.to_atom(booking_id)
+    )
+
     conn
-    |> put_resp_header("Location", "/api/123") # "/api/bookings/" <> booking_id)
+    |> put_resp_header("location", "/api/bookings/" <> booking_id)
     |> put_status(:created)
     |> json(%{msg: "We are processing your request"})
   end
+
   def update(conn, %{"action" => "accept", "username" => username, "id" => _id}) do
     IO.inspect("'#{username}' is accepting a booking request")
     json(conn, %{msg: "We will process your acceptance"})
